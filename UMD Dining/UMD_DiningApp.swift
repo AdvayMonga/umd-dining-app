170d06cd922da6f3128fde2c28b3f91901257d0d
Advay Monga <advaymonga@gmail.com>
2025-11-06 00:12:58 -0500

    Initial Commit

---
+++ UMD Dining/UMD_DiningApp.swift
@@ -0,0 +1,17 @@
+//
+//  UMD_DiningApp.swift
+//  UMD Dining
+//
+//  Created by Advay Monga on 11/6/25.
+//
+
+import SwiftUI
+
+@main
+struct UMD_DiningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
